8fbaf469f385e1962973bece0764438ac8453355
agent-indigo <bradenhynes@gmail.com>
2024-12-30 17:39:55 -0330

    Added Hero block to home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import {
   ReactElement
 } from 'react'
 import {Metadata} from 'next'
+import Hero from '@/components/Hero'
 export const metadata: Metadata = {}
 const HomePage: FunctionComponent = (): ReactElement => (
   <>
-    
+    <Hero/>
   </>
 )
 export default HomePage
